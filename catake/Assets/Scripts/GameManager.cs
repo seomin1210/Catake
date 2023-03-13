ef0324a8a8f8f59c7556cd15b9252c4020a97c6f
seomin1210 <seoyun0700@daum.net>
2023-03-13 23:45:51 +0900

    프레임 수정

--- catake/Assets/Scripts/GameManager.cs
+++ catake/Assets/Scripts/GameManager.cs
@@ -1,19 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameManager : MonoBehaviour
-{
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Application.Quit();
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:e33436d979f7b42f8142d3b652e044197d0d843335e1bf54ad7f14ff3e332fe9
+size 352
